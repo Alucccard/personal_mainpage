02efc3dd11f3036563457ef42c38ce6e2b65f2cc
Alucccard <ai454293996@gmail.com>
2026-04-03 00:56:01 +0800

    payload support

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,32 +1,36 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-// import '@payloadcms/ui/styles.css' // Uncomment this line if `@payloadcms/ui` in `tsconfig.json` points to `/ui/dist` instead of `/ui/src`
-import type { ServerFunctionClient } from 'payload'
+import "@payloadcms/ui/styles.css";
+import type { ServerFunctionClient } from "payload";
 
-import config from '@payload-config'
-import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
-import React from 'react'
+import config from "@payload-config";
+import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
+import React from "react";
 
-import { importMap } from './admin/importMap.js'
-import './custom.scss'
+import { importMap } from "./admin/importMap.js";
+import "./custom.scss";
 
 type Args = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const serverFunction: ServerFunctionClient = async function (args) {
-  'use server'
+  "use server";
   return handleServerFunctions({
     ...args,
     config,
     importMap,
-  })
-}
+  });
+};
 
 const Layout = ({ children }: Args) => (
-  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+  <RootLayout
+    config={config}
+    importMap={importMap}
+    serverFunction={serverFunction}
+  >
     {children}
   </RootLayout>
-)
+);
 
-export default Layout
+export default Layout;
